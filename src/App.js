469fefd84c8ae65c9a66b5644b8189e73f1f4b55
Kurt Vermillion <kurtv0727@gmail.com>
2023-01-24 00:04:34 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -16,19 +16,20 @@ function App() {
   const [projects, setProjects] = useState([]);
   const [editProject, setEditProject] = useState([]);
   const [storeItems, setStoreItems] = useState([]);
-  const projectPics = [{ id:1, category: "Add Ceiling Fan", pics: "./pics/fan.jpg" },
-      { id:2, category: "Change Fan/Light", pics: "./pics/fan.jpg" },
-      { id:3, category: "Receptacle", pics: "./pics/recepacle.jpg" },
-      { id:4, category: "Devices", pics: "./pics/devices.jpg" },
-      { id:5, category: "GFCI", pics: "./pics/gfci-outlet.jpg" },
-      { id:6, category: "Wafer Lights", pics: "./pics/wafer.jpg" },
-      { id:7, category: "Flood Lights", pics: "./pics/flood.jpg" },
-      { id:8, category: "Replace Breaker", pics: "./pics/breaker.jpg" },
-      { id:9, category: "Home Surge Protection", pics: "./pics/surge.jpg" },
-      { id:10, category: "Exterior Receptacle", pics: "./pics/exteriorreceptacle.jpg" }
-    ]
+  const projectPics = [
+    { id: 1, category: "Add Ceiling Fan", pics: "./pics/fan.jpg" },
+    { id: 2, category: "Change Fan/Light", pics: "./pics/fan.jpg" },
+    { id: 3, category: "Receptacle", pics: "./pics/recepacle.jpg" },
+    { id: 4, category: "Devices", pics: "./pics/devices.jpg" },
+    { id: 5, category: "GFCI", pics: "./pics/gfci-outlet.jpg" },
+    { id: 6, category: "Wafer Lights", pics: "./pics/wafer.jpg" },
+    { id: 7, category: "Flood Lights", pics: "./pics/flood.jpg" },
+    { id: 8, category: "Replace Breaker", pics: "./pics/breaker.jpg" },
+    { id: 9, category: "Home Surge Protection", pics: "./pics/surge.jpg" },
+    { id: 10, category: "Exterior Receptacle", pics: "./pics/exteriorreceptacle.jpg" }
+  ];
   const noUsers = user === undefined || user.length === 0;
-  
+
   useEffect(() => {
     fetch("http://127.0.0.1:9292/customers")
       .then((r) => r.json())
@@ -56,8 +57,8 @@ function App() {
   function updateQuantity(patchedItem) {
     const updatedQuantity = storeItems.map((item) =>
       item.id === patchedItem.id ? patchedItem : item
-      );
-      setStoreItems(updatedQuantity)
+    );
+    setStoreItems(updatedQuantity);
   }
   function adminDelete(id) {
     const updatedCustomers = customers.filter((customer) => customer.id !== id);
@@ -81,8 +82,6 @@ function App() {
   }
   function logOut() {
     setUser([]);
-
-
   }
   return (
     <div className="App">
@@ -98,8 +97,6 @@ function App() {
               user={user}
               setUser={setUser}
               noUsers={noUsers}
-              // projects={projects}
-              // setProjects={setProjects}
             />
           }
         />
@@ -108,16 +105,11 @@ function App() {
           element={
             <Customerprojects
               user={user}
-              setUser={setUser}
               projects={projects}
-              setProjects={setProjects}
               noUsers={noUsers}
               handleDelete={handleDelete}
-              editProject={editProject}
               setEditProject={setEditProject}
               projectPics={projectPics}
-
-
             />
           }
         />
@@ -125,14 +117,8 @@ function App() {
           path="/Customerprojects/Edititems"
           element={
             <Edititems
-              user={user}
-              setUser={setUser}
-              noUsers={noUsers}
               editProject={editProject}
-              setEditProject={setEditProject}
               storeItems={storeItems}
-              setStoreItems={setStoreItems}
-              addNewItem={addNewItem}
               handleDeleteItem={handleDeleteItem}
               updateQuantity={updateQuantity}
             />
@@ -142,13 +128,9 @@ function App() {
           path="/Customerprojects/Additems"
           element={
             <Itemslist
-              user={user}
-              setUser={setUser}
-              noUsers={noUsers}
               editProject={editProject}
               setEditProject={setEditProject}
               storeItems={storeItems}
-              setStoreItems={setStoreItems}
               addNewItem={addNewItem}
             />
           }
@@ -172,7 +154,6 @@ function App() {
               noUsers={noUsers}
               addNewProject={addNewProject}
               projects={projects}
-              setProjects={setProjects}
               projectPics={projectPics}
             />
           }
@@ -186,7 +167,4 @@ function App() {
   );
 }
 
-
-
 export default App;
-
